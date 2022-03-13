fa555d7431a94ff9161c5224203ab5b65d4c908d
ericsonmoreira <moreira.ericson@gmail.com>
2022-03-13 11:28:55 -0300

    🎉 inital commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import BasicLayout from "./layouts/BasicLayout";
+
+function App() {
+  return <BasicLayout>Ola mundo</BasicLayout>;
+}
+
+export default App;
---
+++ src/layouts/BasicLayout/index.tsx
@@ -0,0 +1,8 @@
+import { Container } from "./styles";
+
+const BasicLayout: React.FC = (props) => {
+  const { children } = props;
+  return <Container>{children}</Container>;
+};
+
+export default BasicLayout;
